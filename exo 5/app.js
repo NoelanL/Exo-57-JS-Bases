2cf9cff7212cea266219a1172a37f9a16047a1ec
NoelanL <92019519+NoelanL@users.noreply.github.com>
2021-11-15 14:15:41 +0100

    fini exo

---
+++ exo 5/app.js
@@ -0,0 +1,5 @@
+let maVariable = document.getElementById("para");
+let number = prompt("Donnez un nombre à virgule.");
+let number2 = prompt("Donner un deuxiéme nombre à virgule.");
+
+alert("Le resultat de de la multiplication de vos deux nombre choisit est de " + number * number2);
